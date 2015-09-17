dc0ac7e8032cee9a4b0c87a936a450724eed2718
stening <stening.johan@gmail.com>
2015-09-17 21:50:02 +0000

    slutfört labb 3

---
+++ script/testscript.js
@@ -0,0 +1,9 @@
+
+/*var knapp1 = document.getElementById("indexmeny_1");
+var clicksound = new Audio("../sound/clicksound.wav");
+
+knapp1.onclick = function(){
+    clicksound.play();
+    alert("testing!")
+};*/
+
