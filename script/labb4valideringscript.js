f9f220da21b967c91112767d7dfd28710a16349a
stening <stening.johan@gmail.com>
2015-10-02 19:12:45 +0000

    working on 4.2

--- script/labb4valideringscript.js
+++ script/labb4valideringscript.js
@@ -6,20 +6,20 @@ window.onload = function() {
     var postNumberNotice = document.getElementById("postnumbernotice");
     var lastNameNotice = document.getElementById("lastnamenotice");
     var firstNameNotice = document.getElementById("firstnamenotice");
+    var priceModel = document.getElementById("firstnamenotice");
     var nameCheck = /^[a-öA-Ö]+$/;
-    var numbers = /^[0123456789]+$/;
     var emailCheck = /^([a-öA-Ö]+[.][a-öA-Ö]+[@][a-öA-Ö]+[.][a-öA-Ö]+|[a-öA-Ö]+[@][a-öA-Ö]+[.][a-öA-Ö]+)$/;
-    var toggle = true;
-    var postNummerInputsortedTotal = "";
     var postNumberFormat = /^([Ss][Ee][ -]\d{5}|\d{5}|\d{3}[- ]\d{2}|[sS][eE]\d{3}[- ]\d{2}|[Ss][Ee]\d{5}|[Ss][Ee]\d{3}[- ]\d{2}|[Ss][Ee][ -]\d{3}[- ]\d{2})$/;
     var emailInputBox = document.getElementById("emailinputbox");
     var emailNotice = document.getElementById("emailnotice");
     var firstNameValidated = "";
     var lastNameValidated = "";
     var postNumber = "";
-
-
-
+    var popupBox = document.getElementById("popupbox");
+    var inputInfoBox = document.getElementById("inputinfo");
+    var awaitingConfirm = false;
+    var confirmButton = document.getElementById("confirmbutton");
+    var cancelButton = document.getElementById("cancelbutton");
 
     postNumberInputBox.onblur = function() {
         if (postNumberInputBox.value.match(postNumberFormat)) {
@@ -31,23 +31,23 @@ window.onload = function() {
             else {
                 postNumberNotice.insertAdjacentHTML("afterbegin", "<i id='postnumbercheck' class='fa fa-check'></i>");
             }
-            for(var i = 0; i < postNumberInputBox.value.length; i++){
-                switch (postNumberInputBox.value[i]){
-                case " ":
-                case "-":
-                case "S":
-                case "s":
-                case "e":
-                case "E":
-                    postNumber += "";
-                break;
-                default:
-                postNumber += postNumberInputBox.value[i];
+            postNumber = "";
+            for (var i = 0; i < postNumberInputBox.value.length; i++) {
+                switch (postNumberInputBox.value[i]) {
+                    case " ":
+                    case "-":
+                    case "S":
+                    case "s":
+                    case "e":
+                    case "E":
+                        postNumber += "";
+                        break;
+                    default:
+                        postNumber += postNumberInputBox.value[i];
                 }
             }
-            postNumber = postNumberInputBox.value;
+            console.log(postNumber)
             postNumberInputBox.style.backgroundColor = ("#75E376");
-            console.log(postNumber);
         }
         else {
             if ((document.getElementById("postnumbercheck")) || (document.getElementById("postnumbertriangle"))) {
@@ -128,8 +128,7 @@ window.onload = function() {
             }
             else {
                 lastNameNotice.insertAdjacentHTML("afterbegin", "<i id='lastnametriangle' class='fa fa-exclamation-triangle'></i>");
-                 console.log("please enter the proper format!");
-            lastNameInputBox.focus();
+                console.log("please enter the proper format!");
             }
             lastNameInputBox.style.backgroundColor = ("#FF9F9F");
         }
@@ -140,8 +139,8 @@ window.onload = function() {
 
 
 
-emailInputBox.onblur = function(){
-    if (emailInputBox.value.match(emailCheck) && (emailInputBox.value.length < 35)) {
+    emailInputBox.onblur = function() {
+        if (emailInputBox.value.match(emailCheck) && (emailInputBox.value.length < 35)) {
             if ((document.getElementById("emailcheck")) || (document.getElementById("emailtriangle"))) {
                 emailNotice.removeChild(emailNotice.firstChild);
                 emailNotice.insertAdjacentHTML("afterbegin", "<i id='emailcheck' class='fa fa-check'></i>");
@@ -158,32 +157,61 @@ emailInputBox.onblur = function(){
             }
             else {
                 emailNotice.insertAdjacentHTML("afterbegin", "<i id='emailtriangle' class='fa fa-exclamation-triangle'></i>");
-                 console.log("please enter the proper format!");
-            emailInputBox.focus();
+                console.log("please enter the proper format!");
+                emailInputBox.focus();
             }
             emailInputBox.style.backgroundColor = ("#FF9F9F");
         }
         console.log(emailInputBox.value.length);
     };
-    
 
 
 
 
 
 
-setInterval( function(){
-    if((document.getElementById("emailcheck")) && (document.getElementById("postnumbercheck")) && (document.getElementById("lastnamecheck")) && (document.getElementById("firstnamecheck"))){
+
+    setInterval(function() {
+        if ((document.getElementById("emailcheck")) && (document.getElementById("postnumbercheck")) && (document.getElementById("lastnamecheck")) && (document.getElementById("firstnamecheck") && (awaitingConfirm === false))) {
+            submitButton.disabled = false;
+        }
+        else {
+            submitButton.disabled = true;
+        }
+    }, 10);
+
+
+
+    submitButton.onclick = function() {
+        priceModel.disabled = true;
+        emailInputBox.disabled = true;
+        awaitingConfirm = true;
+        firstNameInputBox.disabled = true;
+        lastNameInputBox.disabled = true;
+        postNumberInputBox.disabled = true;
+        popupBox.style.display = "block";
+        inputInfoBox.style.opacity = "0.5";
+        submitButton.disabled = true;
+    };
+    function sitereset(){
+        priceModel.disabled = false;
+        emailInputBox.disabled = false;
+        awaitingConfirm = false;
+        firstNameInputBox.disabled = false;
+        lastNameInputBox.disabled = false;
+        postNumberInputBox.disabled = false;
+        popupBox.style.display = "none";
+        inputInfoBox.style.opacity = "1";
         submitButton.disabled = false;
     }
-    else{
-        submitButton.disabled = true;
-    }
-}, 10);
-
-
-
-
+    
+    confirmButton.onclick = function(){
+        sitereset();
+    };
+    cancelButton.onclick = function(){
+        sitereset();
+    };
+    
 };
 
 
